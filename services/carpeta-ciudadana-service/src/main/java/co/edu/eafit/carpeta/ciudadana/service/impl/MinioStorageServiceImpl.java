47ac03ff861a3a246b89c665c6a298deda6491d0
QuitoTactico <teteban0917@gmail.com>
2025-11-09 12:05:27 -0500

    Remove unused import for Qualifier in MinioStorageServiceImpl

--- services/carpeta-ciudadana-service/src/main/java/co/edu/eafit/carpeta/ciudadana/service/impl/MinioStorageServiceImpl.java
+++ services/carpeta-ciudadana-service/src/main/java/co/edu/eafit/carpeta/ciudadana/service/impl/MinioStorageServiceImpl.java
@@ -5,7 +5,6 @@ import co.edu.eafit.carpeta.ciudadana.service.MinioStorageService;
 import io.minio.*;
 import io.minio.http.Method;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
